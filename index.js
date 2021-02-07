b1019caa51f0ab31eb20e1d969653e2365fd4497
changicho <changicho29@gmail.com>
2021-02-07 22:49:25 +0900

    :art: Apply TextGrid component to root

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const rootEl = document.querySelector("#root");
+import { textToGridArray } from "./utils.js";
+import { TextGrid } from "./TextGrid.js";
+
+const rootElement = document.querySelector("#app");
+
+const text = "helloworld";
+const textGrid = new TextGrid(text, 5, 5);
+const textGridElement = textGrid.getElement();
+
+rootElement.appendChild(textGridElement);
